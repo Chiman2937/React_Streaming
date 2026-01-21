5e11df9e2dcee50af0cddaab9ca4cd1ab4034bdb
Kim Chiyoung <cyga2933@gmail.com>
2026-01-21 16:02:47 +0900

    fix: main.tsx 삭제, entry-client와 entry-server에 providers 추가

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,12 +1,19 @@
-// src/entry-client.tsx
+import './globals.css';
+
+import { StrictMode } from 'react';
 import { hydrateRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import Providers from './providers';
 
 hydrateRoot(
   document.getElementById('root')!,
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <Providers>
+        <App />
+      </Providers>
+    </BrowserRouter>
+  </StrictMode>,
 );
