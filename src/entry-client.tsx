1a1d10c498ae53f54349e5ef488f7fa64fac9963
Chiman2937 <cyga2933@gmail.com>
2026-01-22 22:08:33 +0900

    feat: React Query를 활용한 SSR 환경 구현

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -4,15 +4,27 @@ import { StrictMode } from 'react';
 import { hydrateRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import { type DehydratedState, HydrationBoundary } from '@tanstack/react-query';
+
 import App from './App';
 import Providers from './providers';
+
+declare global {
+  interface Window {
+    __REACT_QUERY_STATE__?: DehydratedState;
+  }
+}
+
+const dehydratedState = window.__REACT_QUERY_STATE__;
 
 hydrateRoot(
   document.getElementById('root')!,
   <StrictMode>
     <BrowserRouter>
       <Providers>
-        <App />
+        <HydrationBoundary state={dehydratedState}>
+          <App />
+        </HydrationBoundary>
       </Providers>
     </BrowserRouter>
   </StrictMode>,
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,16 +1,36 @@
+import { StrictMode } from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+
 import App from './App';
+import { getQueryClient } from './lib/queryClient';
 import Providers from './providers';
 
-export function render(url: string) {
+export async function render(url: string) {
+  const queryClient = getQueryClient();
+
+  // URL별 prefetch
+  if (url === '/') {
+    await queryClient.prefetchQuery({
+      queryKey: ['users'],
+      queryFn: () => fetch('http://localhost:3000/api/users').then((r) => r.json()),
+    });
+  }
+
+  const dehydratedState = dehydrate(queryClient);
+
   const html = renderToString(
-    <StaticRouter location={url}>
-      <Providers>
-        <App />
-      </Providers>
-    </StaticRouter>,
+    <StrictMode>
+      <StaticRouter location={url}>
+        <Providers>
+          <HydrationBoundary state={dehydratedState}>
+            <App />
+          </HydrationBoundary>
+        </Providers>
+      </StaticRouter>
+    </StrictMode>,
   );
-  return html;
+  return { html, dehydratedState };
 }
