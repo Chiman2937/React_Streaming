9de48f00b75abfaeac0276521ef719a8699cc352
Chiman2937 <cyga2933@gmail.com>
2026-01-22 22:09:49 +0900

    delete: MSW 기능 불필요로 providers에서 제거

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,16 +1,10 @@
 import { QueryProvider } from '@/providers/QueryProvider';
 
-import { MSWProvider } from './providers/MSWProvider';
-
 interface Props {
   children: React.ReactNode;
 }
 const Providers = ({ children }: Props) => {
-  return (
-    <MSWProvider>
-      <QueryProvider>{children}</QueryProvider>
-    </MSWProvider>
-  );
+  return <QueryProvider>{children}</QueryProvider>;
 };
 
 export default Providers;
