432b9491a9d362f13eb68734526e227a5b7eb7f2
Chiman2937 <cyga2933@gmail.com>
2026-01-22 22:09:28 +0900

    feat: RootPage 화면 구성

--- src/pages/root/RootPage.tsx
+++ src/pages/root/RootPage.tsx
@@ -1,5 +1,27 @@
+// src/pages/RootPage.tsx
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+interface User {
+  id: number;
+  name: string;
+}
+
 const RootPage = () => {
-  return <div>Root</div>;
+  const { data: users } = useSuspenseQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => fetch('/api/users').then((r) => r.json()),
+  });
+
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user.id}>
+          <span>{user.id}</span>
+          <span>{user.name}</span>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default RootPage;
