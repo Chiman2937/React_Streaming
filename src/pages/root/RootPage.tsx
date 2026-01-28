375c452b9cafbd2763f4d7491a3fc8a24d20cdd8
Chiman2937 <cyga2933@gmail.com>
2026-01-28 23:02:45 +0900

    fix: RootPage 구문 임시 간소화

--- src/pages/root/RootPage.tsx
+++ src/pages/root/RootPage.tsx
@@ -1,26 +1,15 @@
 // src/pages/RootPage.tsx
-import { useSuspenseQuery } from '@tanstack/react-query';
-
-interface User {
-  id: number;
-  name: string;
-}
 
 const RootPage = () => {
-  const { data: users } = useSuspenseQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => fetch('/api/users').then((r) => r.json()),
-  });
+  const buttonClick = () => {
+    console.log('Action');
+  };
 
   return (
-    <div>
-      {users.map((user) => (
-        <div key={user.id}>
-          <span>{user.id}</span>
-          <span>{user.name}</span>
-        </div>
-      ))}
-    </div>
+    <section>
+      <h1>React SSR</h1>
+      <button onClick={buttonClick}>Action Button</button>
+    </section>
   );
 };
 
