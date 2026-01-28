c2fce0c602fbeff0c737582676be4af5698e848d
Chiman2937 <cyga2933@gmail.com>
2026-01-28 23:09:28 +0900

    fix: entry-client, entry-server 구문 간소화

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -2,35 +2,15 @@ import { StrictMode } from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-
 import App from './App';
-import { getQueryClient } from './lib/queryClient';
-import Providers from './providers';
 
 export async function render(url: string) {
-  const queryClient = getQueryClient();
-
-  // URL별 prefetch
-  if (url === '/') {
-    await queryClient.prefetchQuery({
-      queryKey: ['users'],
-      queryFn: () => fetch('http://localhost:3000/api/users').then((r) => r.json()),
-    });
-  }
-
-  const dehydratedState = dehydrate(queryClient);
-
   const html = renderToString(
     <StrictMode>
       <StaticRouter location={url}>
-        <Providers>
-          <HydrationBoundary state={dehydratedState}>
-            <App />
-          </HydrationBoundary>
-        </Providers>
+        <App />
       </StaticRouter>
     </StrictMode>,
   );
-  return { html, dehydratedState };
+  return { html };
 }
